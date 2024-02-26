bc296016dec34c9a0667e8ef8273c2de2177a13d
akshaysubramanian <akshaysubramanian55@gmail.com>
2024-02-26 16:04:07 +0530

    feb26 message

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -12,43 +12,47 @@ function Login() {
         e.preventDefault();
         try {
             const response = await axios.post('http://localhost:3100/login', {
-                email,
-                password
+                email: email,
+                password: password
+            }, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
             });
 
             if (response.data.success) {
                 const token = response.data.data;
                 localStorage.setItem('token', token);
-                // Navigate to 'getuser' route
-                navigate('/getuser');
+                navigate('/adduser');
                 alert(response.data.message);
             } else {
                 alert(response.data.message);
             }
         } catch (error) {
             console.error('Login failed:', error);
-            // Handle login failure, e.g., show error message to the user
+            alert('Login failed. Please try again later.');
         }
     };
 
-  
+
 
 
     return (
         <div className='logindataa' >
-          
+
             <h2>Login</h2>
             <form className='landinglogins' onSubmit={handleSubmit} >
-                
+
                 <label htmlFor="name">Enter your email</label>
                 <input type="email" placeholder="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
                 <label htmlFor="password">Enter Your Password</label>
                 <input type="password" placeholder="Password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
 
                 <div className='centre'>
-              <button type='submit'>Login</button>  
+                    <button type='submit'>Login</button>
                 </div>
-                
+
             </form>
         </div>
     );
