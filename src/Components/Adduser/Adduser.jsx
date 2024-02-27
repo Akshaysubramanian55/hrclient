457ca5072b3a718351f7d5dcd4564b1a809530d6
akshaysubramanian <akshaysubramanian55@gmail.com>
2024-02-27 15:51:40 +0530

    feb27client  message

--- src/Components/Adduser/Adduser.jsx
+++ src/Components/Adduser/Adduser.jsx
@@ -1,38 +1,74 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import './Adduser.css'
 
-function Adduser(){
-    const [username, setUsername] = useState('');
+function Adduser() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('');
-    const [phonenumber,setPhonenumber] =useState('');
-    const [address,setAddress] =useState('');
-    const [pincode,setPincode] =useState('');
+    const [phonenumber, setPhonenumber] = useState('');
+    const [Address, setAddress] = useState('');
+    const [pincode, setPincode] = useState('');
 
-return(
+    const handleAdduser = async (e) => {
+
+        try {
+
+            const data = { name, email, password, phonenumber, Address, pincode };
+            const json_data = JSON.stringify(data);
+            console.log("json_data : ", json_data)
+
+            const response = await fetch('http://localhost:3100/adduser', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: json_data,
+            });
+
+            if (response.message='success') {
+                
+                alert(response.message);
+            } else {
+                alert(response.message);
+            }
+        } catch (error) {
+            console.error('Adding user failed:', error);
+            alert(' failed. Please try again later.')
+        }
+    }
+
+    return (
 
 
-    <div className='adddata' >
-          
+        <div className='adddata' >
+
             <h2>Add-User</h2>
-            <form className='adddetails'>
+            <form className='adddetails' onSubmit={handleAdduser}>
                 <label htmlFor="name">Enter your UserName</label>
-                <input type="text" placeholder="Username" name='name' value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" placeholder="Username" name='name' value={name} onChange={(e) => setName(e.target.value)} />
+
+                <label htmlFor="email">Enter your Email</label>
+                <input type="email" placeholder="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+
                 <label htmlFor="password">Enter Your Password</label>
                 <input type="password" placeholder="Password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+
                 <label htmlFor="phonenumber">Enter Your Phone Number</label>
-                <input type="text" placeholder="Enter Your Phone Number" name="phonenumber" value={phonenumber}  onChange={(e)=> setPhonenumber(e.target.value)} />
+                <input type="text" placeholder="Enter Your Phone Number" name="phonenumber" value={phonenumber} onChange={(e) => setPhonenumber(e.target.value)} />
+
                 <label htmlFor="address">Enter Your Address</label>
-                <input type="address" placeholder="Enter Your Address" name="address" value={address}  onChange={(e)=> setAddress(e.target.value)} />
+                <input type="address" placeholder="Enter Your Address" name="Address" value={Address} onChange={(e) => setAddress(e.target.value)} />
+
                 <label htmlFor="pincode">Enter Your Pincode</label>
-                <input type="pincode" placeholder="Enter Your pincode" name="pincode" value={pincode}  onChange={(e)=> setPincode(e.target.value)} />
+                <input type="pincode" placeholder="Enter Your pincode" name="pincode" value={pincode} onChange={(e) => setPincode(e.target.value)} />
 
                 <div className='centre'>
-                <button type="submit">Login</button>
+                    <button type="submit" >Add User</button>
                 </div>
-                
+
             </form>
         </div>
-)
+    )
 
 }
 export default Adduser;
