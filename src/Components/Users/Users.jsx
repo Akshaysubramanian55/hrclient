c71a2e5b62ec5af3c43e1ed9aefa3a7b06502368
akshaysubramanian <akshaysubramanian55@gmail.com>
2024-03-15 15:50:59 +0530

    march15 commit

---
+++ src/Components/Users/Users.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+return (
+
+    <div>
+
+        <div>
+            <h1>Hello</h1>
+        </div>
+        <div>
+            <p>This is your profile page.</p>
+        </div>
+        <div>
+            <button>Edit Profile</button>
+        </div>
+    </div>
+)
